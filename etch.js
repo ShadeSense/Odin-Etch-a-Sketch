7d0c5d3f04372153ede4197e718ada29435946b9
ShadeSense <songlcharles@gmail.com>
2022-03-21 09:35:46 -0500

    Add event listener for coloring

--- etch.js
+++ etch.js
@@ -4,6 +4,10 @@ for(let i = 0; i < 10000; i++){
     const gridEle = document.createElement("div");
     gridEle.classList.add("grid-element");
     container.appendChild(gridEle);
+
+    gridEle.addEventListener("mouseover", () => {
+        gridEle.style.backgroundColor="red";
+    });
 }
 
 /* Need to change grid-template-column & row in css with js */
