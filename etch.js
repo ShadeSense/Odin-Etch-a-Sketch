cee0edc0a2209ca249b4b790ab80f91274400e25
ShadeSense <songlcharles@gmail.com>
2022-03-21 09:42:30 -0500

    Add clear button and event listener for clearing grid

--- etch.js
+++ etch.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container");
+const btnClear = document.querySelector("#btn-clear");
 
 for(let i = 0; i < 10000; i++){
     const gridEle = document.createElement("div");
@@ -7,7 +8,15 @@ for(let i = 0; i < 10000; i++){
 
     gridEle.addEventListener("mouseover", () => {
         gridEle.style.backgroundColor="red";
+
+        btnClear.addEventListener("click", () => {
+            gridEle.style.backgroundColor="white";
+        });
     });
+
+
 }
 
 /* Need to change grid-template-column & row in css with js */
+
+/* Add prompt for confirmation for buttons */
